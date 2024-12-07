db802ba359eba3834de5e36da452d98afbb104cd
AGunich <138204726+cartierishere@users.noreply.github.com>
2024-12-07 22:04:20 +0300

    added new @Bean for modelMapper

--- src/main/java/com/alexgunich/AutoHubApplication.java
+++ src/main/java/com/alexgunich/AutoHubApplication.java
@@ -1,7 +1,9 @@
 package com.alexgunich;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AutoHubApplication {
@@ -10,4 +12,8 @@ public class AutoHubApplication {
 		SpringApplication.run(AutoHubApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 }
